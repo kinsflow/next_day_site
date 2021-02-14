4616b12f9287ac384e01eaea248fb2e8038f35fd
kinsflow <kingsley.davidakindele@gmail.com>
2021-02-14 20:55:41 +0100

    modify assignment 3

--- comp_assignment_3/app/Http/Controllers/UserController.php
+++ comp_assignment_3/app/Http/Controllers/UserController.php
@@ -18,10 +18,10 @@ class UserController extends Controller
 
     private function handleSearch($search_value)
     {
-        $search = User::where("first_name", "like", "%" . $search_value . "%")
-            ->orWhere("last_name", "like", "%" . $search_value . "%")
-            ->orWhere("email", "like", "%" . $search_value . "%")
-            ->orWhere("phone_number", "like", "%" . $search_value . "%")
+        $search = User::where("first_name", "like", $search_value . "%")
+            ->orWhere("last_name", "like", $search_value . "%")
+            ->orWhere("email", "like", $search_value . "%")
+            ->orWhere("phone_number", "like", $search_value . "%")
             ->paginate();
         return $search;
     }
